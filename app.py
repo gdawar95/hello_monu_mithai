33b515da9fb45d52c371999086640bc0e007cdeb
Gautam Dawar <55209197+gdawar95@users.noreply.github.com>
2024-03-12 22:08:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,5 @@ import streamlit as st
 def main():
     st.title("Welcome to Monu Mithai")
     st.write("Hello Monu Mithai")
-    st.write("Yes this is my way of flirting - sort of")
-
 if __name__ == "__main__":
     main()
